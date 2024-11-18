90315b2e230ba19f96eb4c9ce8c82baccf529e2f
Jean Carlos <jcarlos.ordonez01@gmail.com>
2024-11-18 11:48:48 -0500

    fix: load project image

--- gallery/urls.py
+++ gallery/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', index, name='gallery'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,
-                      document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
